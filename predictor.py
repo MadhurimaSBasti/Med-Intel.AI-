02aaea4ea3fe08409a35b27156458e986502c875
Madhurima Basti <bastimadhurima@gmail.com>
2026-03-04 22:56:02 +0530

    Fix Streamlit BigQuery authentication

--- predictor.py
+++ predictor.py
@@ -1,48 +1,12 @@
+import streamlit as st
+import json
+from google.oauth2 import service_account
 from google.cloud import bigquery
 
-client = bigquery.Client()
+service_account_info = json.loads(st.secrets["gcp_service_account"]["json"])
 
-# Diabetes Prediction
-def predict_diabetes(age, bmi, bp, glucose):
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info
+)
 
-    query = f"""
-    SELECT
-      predicted_diabetes,
-      predicted_diabetes_probs
-    FROM ML.PREDICT(
-      MODEL `hospital-160e7.healthcare.diabetes_model`,
-      (
-        SELECT
-          {age} AS age,
-          {bmi} AS bmi,
-          {bp} AS blood_pressure,
-          {glucose} AS glucose
-      )
-    )
-    """
-
-    result = client.query(query).to_dataframe()
-    return result
-
-
-# Heart Disease Prediction
-def predict_heart(age, bp, chol, hr):
-
-    query = f"""
-    SELECT
-      predicted_HeartDisease,
-      predicted_probability
-    FROM ML.PREDICT(
-      MODEL `hospital-160e7.healthcare.heart_model`,
-      (
-        SELECT
-          {age} AS age,
-          {bp} AS RestingBP,
-          {chol} AS Cholesterol,
-          {hr} AS MaxHR
-      )
-    )
-    """
-
-    result = client.query(query).to_dataframe()
-    return result
+client = bigquery.Client(credentials=credentials, project="hospital-160e7")
