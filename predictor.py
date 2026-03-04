95c95971803e29b5af05211bc47860ac3fbb4c6a
Madhurima Basti <bastimadhurima@gmail.com>
2026-03-04 23:01:11 +0530

    Fix predictor functions

--- predictor.py
+++ predictor.py
@@ -10,3 +10,47 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 
 client = bigquery.Client(credentials=credentials, project="hospital-160e7")
+
+
+def predict_diabetes(age, bmi, bp, glucose):
+
+    query = f"""
+    SELECT
+      predicted_diabetes,
+      predicted_diabetes_probs
+    FROM ML.PREDICT(
+      MODEL `hospital-160e7.healthcare.diabetes_model`,
+      (
+        SELECT
+          {age} AS age,
+          {bmi} AS bmi,
+          {bp} AS blood_pressure,
+          {glucose} AS glucose
+      )
+    )
+    """
+
+    result = client.query(query).to_dataframe()
+    return result
+
+
+def predict_heart(age, bp, chol, hr):
+
+    query = f"""
+    SELECT
+      predicted_HeartDisease,
+      predicted_HeartDisease_probs
+    FROM ML.PREDICT(
+      MODEL `hospital-160e7.healthcare.heart_model`,
+      (
+        SELECT
+          {age} AS age,
+          {bp} AS RestingBP,
+          {chol} AS Cholesterol,
+          {hr} AS MaxHR
+      )
+    )
+    """
+
+    result = client.query(query).to_dataframe()
+    return result
